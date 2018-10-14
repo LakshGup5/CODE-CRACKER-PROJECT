c7a440eb9f9e5263ebe928d3a0e9d1ed72bb0cf2
LakshGup5 <41245898+LakshGup5@users.noreply.github.com>
2018-10-14 10:53:37 -0700

    Updated
    Added Comments

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-import random   
-import time 
+#Importing Modules
+import random
+import time
+
+#Defining Functions
 def factorial(n):
     if n == 0:
-        return 1
+        return True
     else:
         return n * factorial(n-1)
 print('Welcome to Code Cracker')
-lives = 126        
 def even(num):
         if num % 2 == 0:
                 return True
@@ -32,39 +34,42 @@ def product(password):
         for x in password:
                 producty *= int(x)
         return producty  
-     
+
+#Finally Displaying
 integer = random.randint(3, 5)
 chars= "1234567890"
 password = random.sample(chars, integer)
 #function to calculate sum
 lives = factorial(integer)
 _sum_ = Sum(password)
-print('loding...')
+print('loading...')
 time.sleep(random.randint(0, 5))
 password = ''.join(password)
 print("CODE GENARATED")
-print('It is', integer ,'numbers long.')
-print('The sum is', _sum_, '.')
+print('it is', integer ,'numbers long')
+print('the sum is', _sum_)
 producty = product(password)
-print('The product is', producty, '.')
+print('the product is', producty)
 isEven = countEven(password)
-print('The number of even numbers is', isEven, '.')
-print('The first number is', int(password[0], '.'))
-print('The last number is', int(password[-1], '.'))
+print('The number of even numbers is', isEven)
+print('The first number is', int(password[0]))
+print('The last number is', int(password[-1]))
 isOdd = integer - isEven
-print('The number of odd numbers is', isOdd, '.')
-print('The highest number is', str(max(password)), '.')
-print('The lowest number is', str(min(password)), '.')
-print('You have', lives, 'lives.')
+print('The number of odd numbers is', isOdd)
+print('The highest number is', str(max(password)))
+print('The lowest number is', str(min(password)))
+print('you have', lives, 'lives')
+
+#Checking if the player has any lives left. If not, it breaks the program, stopping the game.
 while lives > 0:
-        answer = input('What is your password?')
+        answer = input('what is your password')
         if answer == password:
                         print('OMG, you got it ')
-                        print('The password was', password, '.')
+                        print('The password was', password)
                         break
         if answer != password:
                 lives -= 1
-                print('You have', lives, 'lives.')
+                print('you have', lives, 'lives')
                 print('BOO')
                 if lives <= 1:
                         print ('GAME OVER!!!!!')
